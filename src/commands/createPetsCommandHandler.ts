43dc90063cd3bd57f021acec1800d96d1ac7c26e
rentu <5545529+SLdragon@users.noreply.github.com>
2023-05-19 09:36:52 +0800

    Fix typo in createPetsCommandHandler

--- src/commands/createPetsCommandHandler.ts
+++ src/commands/createPetsCommandHandler.ts
@@ -5,7 +5,7 @@ import {
   TriggerPatterns,
   MessageBuilder,
 } from "@microsoft/teamsfx";
-import createPetsResponseCard from "../adaptiveCards/createPetsResponseCard.json";
+import createPetsRequestCard from "../adaptiveCards/createPetsRequestCard.json";
 
 export class CreatePetsCommandHandler implements TeamsFxBotCommandHandler {
   triggerPatterns: TriggerPatterns = "POST /pets$";
@@ -15,6 +15,6 @@ export class CreatePetsCommandHandler implements TeamsFxBotCommandHandler {
     message: CommandMessage,
   ): Promise<string | Partial<Activity> | void> {
     console.log("Bot received message: " + message.text);
-    return MessageBuilder.attachAdaptiveCardWithoutData(createPetsResponseCard);
+    return MessageBuilder.attachAdaptiveCardWithoutData(createPetsRequestCard);
   }
 }
